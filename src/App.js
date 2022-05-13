6db454e772278ca97e7751af3179f06906574231
gunde Anjaneyulu <Anjaneyulugoud123@outlook.com>
2022-05-13 23:04:06 +0530

    install below pakages  and unnecessary code is removed npm install bootstrap --save npm install --save react-router-dom npm install formik --save npm install yup --save

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Login from "./Pages/Login/Loginpage.js";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container'>
+    <BrowserRouter>
+    <Routes>
+    <Route  path="/" element ={<Login/>} />
+    </Routes>
+    </BrowserRouter>
+   </div>
   );
 }
 
---
+++ src/Pages/Login/Loginpage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Loginstyle from "./Loginstyle.css";
+
+
+const Login = () => {
+    return (
+        <div>
+            <h3> wellcome login page</h3>
+        </div>
+    );
+}
+
+export default Login;
