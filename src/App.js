ec9d56784af411185f0bd06c4eafe18bed69e47d
venkatmanikanta516 <venkat.purushothapu@gmail.com>
2022-05-18 23:05:00 +0530

    added new changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from "./Pages/Login/Loginpage.js";
+import Forget from "./Pages/Forget/Forgetpage.js";
 function App() {
   return (
     <div className='container'>
     <BrowserRouter>
     <Routes>
     <Route  path="/" element ={<Login/>} />
+    <Route path="/Forget" element={<Forget />}></Route>
+
     </Routes>
     </BrowserRouter>
    </div>
--- src/Pages/Forget/Forgetpage.js
+++ src/Pages/Forget/Forgetpage.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Loginstyle from "./Loginstyle.css";
-import { useNavigate,Link } from 'react-router-dom';
+import Forgetstyle from "./Forgetstyle.css";
 
 
 const Login = () => {
@@ -8,19 +7,17 @@ const Login = () => {
         
 <div className='row justify-content-md-center card-height top-pad'>
 <div className="card col col-lg-8 ">
-
+  
   <div className="card-body bg-color">
- 
 <div className='row card-top'>
- 
 <div className="mb-6 row justify-content-md-center txt-top">
-    <label for="inputPassword" className="col-sm-2 col-form-label">User Name</label>
+    <label for="inputPassword" className="col-sm-2 col-form-label">Password</label>
     <div className="col-sm-6">
       <input type="password" className="form-control" id="username"/>
     </div>
   </div>
   <div className="mb-6 row justify-content-md-center">
-    <label for="inputPassword" className="col-sm-2 col-form-label">Password</label>
+    <label for="inputPassword" className="col-sm-2 col-form-label">Confirm Password</label>
     <div className="col-sm-6 txt-margin">
       <input type="password" className="form-control" id="inputPassword"/>
     </div>
@@ -29,11 +26,10 @@ const Login = () => {
         </div>
         <div className="mb-12 row justify-content-md-center txt-margin">
         <label for="inputPassword" className="col-sm-1 col-form-label"></label>
-        <button type="submit" id="" className="btn btn-primary col-lg-3">Login</button>
+        <button type="button" className="btn btn-primary col-lg-3">Login</button>
     
     </div>
-    <Link to="/Forgetpage" className=''> Forget Password</Link>
-       
+        
   </div>
         
   </div>
