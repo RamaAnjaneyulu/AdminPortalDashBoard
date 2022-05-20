53ba031f05e4d3525ddd0fcfa1537348dcbf9176
gunde Anjaneyulu <Anjaneyulugoud123@outlook.com>
2022-05-20 17:35:05 +0530

    added notfoundpage js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Loginpage from "./Pages/Login/Loginpage.js";
 import Forgetpage from './Pages/Forget/Forgetpage.js';
+import NotFoundPage from './Pages/NotFound/NotFoundPage.js';
 function App() {
   return (
     <div className='container'>
@@ -9,6 +10,8 @@ function App() {
     <Routes>
     <Route  path="/" element ={<Loginpage/>} />
     <Route path="/Forgetpage" element={<Forgetpage />}></Route>
+    <Route path="*" element={<NotFoundPage/>}/>
+
     </Routes>
     </BrowserRouter>
    </div>
---
+++ src/Pages/NotFound/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div className="col-sm-5 offset-4">
+              <h1>404 Error!</h1>
+        </div>
+    );
+}
+
+export default NotFoundPage;
